e92f9def6f7ab58fd42e7e045ccb4ad958bf60dc
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-11-07 19:55:07 +0600

    problem 3 solved

---
+++ src/Problem-3.ts
@@ -0,0 +1,9 @@
+function countWordOccurrences(sentence: string, word: string): number {
+    const lowerCaseSentence = sentence.toLowerCase();
+    const lowerCaseWord = word.toLowerCase();
+
+    const words = lowerCaseSentence.split(' ');
+    return words.filter(word => word === lowerCaseWord).length;
+}
+
+console.log(countWordOccurrences("I love typescript", "typescript")); 
